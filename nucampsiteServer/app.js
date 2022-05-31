e9a0769bdbb30892821ac3e98b0f8edf759a3a32
Travis Desktop <traviswilley24@yahoo.com>
2022-05-31 09:27:42 -0700

    HTTPS

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -15,17 +15,33 @@ const mongoose = require('mongoose');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-connect.then(() => console.log('Connected correctly to server'),
-    err => console.log(err)
+connect.then(
+  () => console.log('Connected correctly to server'),
+  (err) => console.log(err)
 );
 
 var app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get('secPort')}${req.url}`
+    );
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,12 +64,12 @@ app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
